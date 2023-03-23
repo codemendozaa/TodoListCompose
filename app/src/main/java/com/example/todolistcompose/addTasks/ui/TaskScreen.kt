9096a591d83bd1993f34182e5c5523d8c546852f
erixjavier <Mendhoza123#>
2023-03-23 14:31:12 -0500

    Implementando Dagger Hilt

--- app/src/main/java/com/example/todolistcompose/addTasks/ui/TaskScreen.kt
+++ app/src/main/java/com/example/todolistcompose/addTasks/ui/TaskScreen.kt
@@ -1,7 +1,8 @@
 package com.example.todolistcompose.addTasks
 
 import androidx.compose.runtime.Composable
+import com.example.todolistcompose.addTasks.ui.TasksViewModel
 
 @Composable
-fun TasksScreen() {
+fun TasksScreen(tasksViewModel: TasksViewModel) {
 }

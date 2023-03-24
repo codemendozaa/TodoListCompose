817454db121c02ad4f26b3a64dca9b916fa61a3f
erixjavier <Mendhoza123#>
2023-03-24 13:39:42 -0500

    Creando las tareas

--- app/src/main/java/com/example/todolistcompose/addTasks/ui/TaskScreen.kt
+++ app/src/main/java/com/example/todolistcompose/addTasks/ui/TaskScreen.kt
@@ -1,7 +1,10 @@
 package com.example.todolistcompose.addTasks
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -14,10 +17,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import com.example.todolistcompose.addTasks.ui.TasksViewModel
+import com.example.todolistcompose.addTasks.ui.model.TaskModel
 
 @Composable
 
@@ -28,7 +33,40 @@ fun TasksScreen(tasksViewModel: TasksViewModel) {
     Box(modifier = Modifier.fillMaxSize()){
         AddTasksDialog(show = showDialog, onDismiss = {tasksViewModel.onDialogClose()}, onTaskAdded = {tasksViewModel.onTaskCreate(it)} )
         FabDialog(Modifier.align(Alignment.BottomEnd),tasksViewModel)
+        TasksList(tasksViewModel)
     }
+}
+
+@Composable
+fun TasksList(tasksViewModel: TasksViewModel) {
+    val myTasks: List<TaskModel> = tasksViewModel.tasks
+    LazyColumn(){
+        items(myTasks, key = {it.id}){ task->
+            ItemTask(taskModel = task, tasksViewModel = tasksViewModel)
+        }
+    }
+}
+
+
+@Composable
+fun ItemTask(taskModel:TaskModel, tasksViewModel: TasksViewModel) {
+    Card(
+        Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+             elevation = 8.dp
+    ) {
+        Row(Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                text = taskModel.task,
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(horizontal = 4.dp))
+            Checkbox(checked = taskModel.selected, onCheckedChange = {tasksViewModel.onCheckBoxSelected(taskModel) })
+
+        }
+    }
+
 }
 
 @Composable
@@ -64,6 +102,7 @@ fun AddTasksDialog(show:Boolean,onDismiss:()->Unit, onTaskAdded:(String)->Unit){
                 Spacer(modifier = Modifier.size(16.dp))
                 Button(onClick = { //enviar tarea
                                  onTaskAdded(myTask)
+                                 myTask = ""
                  },Modifier.fillMaxWidth()) {
                     Text(text = "Enviar Tarea")
                 }
